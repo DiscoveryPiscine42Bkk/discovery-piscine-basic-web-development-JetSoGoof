e64cc9b92f1f9813ddac82c25aeab0c83b100a20
JetSoGoof <comes1177@gmail.com>
2025-06-15 08:49:13 +0000

    build

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 
+# Check if no argument are supplied
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
-else
+  exit1
+fi
+
+# Create dirrectories with prefix "ex"
   for arg in "$@"; do
     mkdir -p "ex$arg"
   done
-fi
